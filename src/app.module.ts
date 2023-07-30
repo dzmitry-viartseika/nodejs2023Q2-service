e2ac5ae261c1cd40e4905ebcccdf6b0d5470540a
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-07-30 20:03:25 +0200

    add favs service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AlbumModule } from './album/album.module';
-import { ArtistModule } from './artist/artist.module';
 import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [AlbumModule, ArtistModule, UserModule],
+  imports: [
+    UserModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
