ea7a94c80a9ff843e41256e6ed323b2e1a020947
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-07-30 19:23:10 +0200

    add user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AlbumModule, ArtistModule],
+  imports: [AlbumModule, ArtistModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
